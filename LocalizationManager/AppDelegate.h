0d9d5e8f1df267cad4c4e1bd3712fd3567628111
Francesca Corsini <francesca.corsini@wuerth-phoenix.com>
2015-05-14 18:31:18 +0200

    Initial Commit

---
+++ LocalizationManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocalizationManager
+//
+//  Created by Francesca Corsini on 14/05/15.
+//  Copyright (c) 2015 Francesca Corsini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LocalizationManager/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LocalizationManager
+//
+//  Created by Francesca Corsini on 14/05/15.
+//  Copyright (c) 2015 Francesca Corsini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
